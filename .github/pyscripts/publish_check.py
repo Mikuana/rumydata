50ed5c8fd11ff283c7350b430548057b4fbb4aaa
Christopher Boyd <mikuana@gmail.com>
2020-07-14 00:29:30 -0700

    Update github action to use same version read as setup

--- .github/pyscripts/publish_check.py
+++ .github/pyscripts/publish_check.py
@@ -1,4 +1,5 @@
 import json
+import re
 import urllib.request
 from pathlib import Path
 
@@ -11,10 +12,20 @@ except ImportError:
 
 from distutils.version import LooseVersion
 
+
+def read_version():
+    try:
+        v = [x for x in Path('rumydata/__init__.py').open() if x.startswith('__version__')][0]
+        v = re.match(r"__version__ *= *'(.*?)'\n", v)[1]
+        return v
+    except Exception as e:
+        raise RuntimeError(f"Unable to read version string: {e}")
+
+
 pypi_url = f'https://pypi.org/pypi/rumydata/json'
 response = urllib.request.urlopen(pypi_url).read().decode()
 latest_version = max(LooseVersion(s) for s in json.loads(response)['releases'].keys())
-setup_version = json.loads(Path('setup.json').read_text()).get('version')
+setup_version = read_version()
 
 if parse(setup_version) > parse(str(latest_version)):
     print(f'PASS: version {setup_version} exceeds latest published, {latest_version}')
