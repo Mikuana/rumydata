a2e4989def75b24bc5cdb34dfbcf579c363c43c3
Christopher Boyd <mikuana@gmail.com>
2020-09-02 20:28:23 -0700

    File name patterns not currently implemented
    Removing, as it was untested code.

--- rumydata/rules/table.py
+++ rumydata/rules/table.py
@@ -5,10 +5,8 @@ These rules are applied to entire files and are generally not meant to be used
 directly. These accomplish things like confirming a file exists, that it matches
 a particular regex pattern, etc.
 """
-
-import re
 from pathlib import Path
-from typing import Union, List
+from typing import Union
 
 from rumydata._base import _BaseRule
 
@@ -28,40 +26,6 @@ class FileExists(Rule):
 
     def _explain(self) -> str:
         return 'files must exist'
-
-
-class FileNameMatchesPattern(Rule):
-    """ File name matches regex pattern Rule """
-
-    _default_args = (re.compile(r'x'),)
-
-    def __init__(self, pattern: Union[re.Pattern, List[re.Pattern]]):
-        super().__init__()
-        self.patterns = [pattern] if isinstance(pattern, re.Pattern) else pattern
-
-    def _evaluator(self):
-        return lambda x: any([p.fullmatch(Path(x).name) for p in self.patterns])
-
-    def _explain(self) -> str:
-        return 'files name must match a pattern provided in the layout'
-
-
-class FileNameMatchesOnePattern(Rule):
-    """ File name matches exactly one pattern Rule """
-
-    _default_args = (re.compile(r'x'),)
-
-    def __init__(self, patterns: list):
-        super().__init__()
-        self.patterns = patterns
-
-    def _evaluator(self):
-        return lambda x: sum([
-            True if p.fullmatch(Path(x).name) else False for p in self.patterns
-        ]) <= 1
-
-    def _explain(self) -> str:
-        return 'files cannot match multiple patterns provided in the layout'
 
 
 class MaxError(Rule):
