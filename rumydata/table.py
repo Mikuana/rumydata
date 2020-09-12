532a7ff98f53fa082f1a74823db79fda834fb93b
Christopher Boyd <mikuana@gmail.com>
2020-09-12 09:45:43 -0700

    Set up file validators as dedicated classes

--- rumydata/table.py
+++ rumydata/table.py
@@ -6,12 +6,14 @@ This submodule contains the File class, and it's closely related Layout class.
 
 import csv
 from pathlib import Path
-from typing import Union, Dict, List
+from typing import Union, Dict, List, Iterable
 
 from rumydata import exception as ex
 from rumydata import field
 from rumydata._base import _BaseSubject
 from rumydata.rules import table, column as cr, header as hr, row as rr, cell as clr
+
+__all__ = ['Layout', 'CsvFile', 'ExcelFile']
 
 
 class Layout(_BaseSubject):
@@ -148,9 +150,9 @@ class Layout(_BaseSubject):
                 return ex.RowError(rix, errors=e)
 
 
-class File(_BaseSubject):
+class _BaseFile(_BaseSubject):
     """
-    File class
+    Base File class
 
     This class provides a way to validate the contents of a file against a
     Layout, and report any rule violations that exist. This is the primary
@@ -159,42 +161,19 @@ class File(_BaseSubject):
     :param layout: a Layout object which defines the fields that make up the
         data set, along with the various rules that should be applied to
         each one.
-    :param skip_rows: (optional) the number of rows to skip before starting
-        evaluation. Defaults to 0.
-    :param max_errors: (optional) the maximum number of row errors to be
+    :param skip_rows: the number of rows to skip before starting evaluation.
+    :param max_errors: the maximum number of row errors to be
         collected before halting validation of rows and raising a FileError.
         This is used to prevent overly verbose (and mostly useless)
-        validation reports from being generated. Defaults to 100. The error
-        limit can overwritten (set to unlimited) by providing a value of -1.
-    :param file_type: (optional) the type of file to be evaluated. Valid
-        values are ['csv', 'excel']. Defaults to 'csv'.
-    :param dialect: (optional) only valid with csv file type. Controls csv
-        dialect parsing.
-    :param delimiter: (optional) only valid with csv file type. Controls csv
-        delimiter parsing.
-    :param quotechar: (optional) only valid with csv file type. Controls csv
-        quote character parsing.
-    :param sheet: (optional) only valid with excel file type. Determines
-        which sheet will be read for tabular data evaluation.
+        validation reports from being generated. The error limit can be set to
+        unlimited by providing a value of -1.
     """
 
-    def __init__(self, layout: Union[Layout, Dict], **kwargs):
-        self.max_errors = kwargs.pop('max_errors', 100)
-        self.file_type = kwargs.pop('file_type', 'csv')
-
-        if self.file_type == 'csv':
-            x = {x: kwargs.pop(x, None) for x in ['dialect', 'delimiter', 'quotechar']}
-            self.csv_kwargs = {k: v for k, v in x.items() if v}
-        elif self.file_type == 'excel':
-            x = {x: kwargs.pop(x, None) for x in ['sheet']}
-            self.excel_kwargs = {k: v for k, v in x.items() if v}
-        else:
-            raise TypeError(f'Invalid file type: {self.file_type}')
-
-        self.skip_rows = kwargs.pop('skip_rows', 0)
+    def __init__(self, layout: Layout, skip_rows=0, max_errors=100, **kwargs):
+        self.skip_rows = skip_rows
+        self.max_errors = max_errors
 
         super().__init__(**kwargs)
-
         self.layout = Layout(layout) if isinstance(layout, Dict) else layout
         self.rules.extend([
             table.FileExists()
@@ -211,7 +190,7 @@ class File(_BaseSubject):
         :param doc_type: the type of output to return with exception details,
             rather than raising an exception. Valid options are ['md', 'html']
         """
-        errors = self._check(file_path)
+        errors = self._check(Path(file_path))
         try:
             assert not errors, str(errors)
             msg = f"Validated file successfully: {Path(file_path).as_posix()}"
@@ -231,11 +210,18 @@ class File(_BaseSubject):
         else:
             raise TypeError(f"Invalid doc type: {doc_type}")
 
+    def _rows(self, file: Path):
+        pass
+
+    @staticmethod
+    def _row_handler(row: list) -> List[str]:
+        return row
+
     def _check(self, filepath: Union[str, Path], **kwargs) -> Union[ex.FileError, None]:
         p = Path(filepath) if isinstance(filepath, str) else filepath
         e = super()._check(p, rule_type=table.Rule)  # check files-based rules first
         if e:
-            return ex.FileError(file=filepath, errors=e)
+            return ex.FileError(file=filepath.as_posix(), errors=e)
 
         column_cache = {
             k: [] for k, v in self.layout.layout.items()
@@ -246,27 +232,12 @@ class File(_BaseSubject):
             for k in column_cache.keys()
         }
 
-        with open(p, newline='') as f:
-            if self.file_type == 'csv':
-                generator = csv.reader(f, **self.csv_kwargs)
-
-                def row_handler(r):  # handle csv rows as-is
-                    return r
-            elif self.file_type == 'excel':
-                from openpyxl import load_workbook
-                wb = load_workbook(p)
-                sheet = self.excel_kwargs.get('sheet')
-                ws = wb.get_sheet_by_name(sheet) if sheet else wb.active
-                generator = ws.values
-
-                def row_handler(r):  # convert values to strings and replace None with empty
-                    return [str(x or '') for x in r]
-
-            max_error_rule = table.MaxError(self.max_errors)
+        max_error_rule = table.MaxError(self.max_errors)
+        with self._rows(filepath) as generator:
             for rix, row in enumerate(generator):
                 if rix < self.skip_rows:
                     continue
-                row = row_handler(row)
+                row = self._row_handler(row)
                 rt = hr.Rule if rix == (0 + self.skip_rows) else rr.Rule
                 re = self.layout._check(row, rule_type=rt, rix=rix)
                 if re:
@@ -280,11 +251,111 @@ class File(_BaseSubject):
                     for k, ix in column_cache_map.items():
                         column_cache[k].append(row[ix])
 
-        for k, v in column_cache.items():
-            ce = self.layout.layout[k]._check(
-                v, cix=column_cache_map[k], rule_type=cr.Rule, name=k
+            for k, v in column_cache.items():
+                ce = self.layout.layout[k]._check(
+                    v, cix=column_cache_map[k], rule_type=cr.Rule, name=k
+                )
+                if ce:
+                    e.append(ce)
+            if e:
+                return ex.FileError(file=p.name, errors=e)
+
+
+class CsvFile(_BaseFile):
+    """
+    CSV File class
+
+    This class provides a way to validate the contents of a file against a
+    Layout, and report any rule violations that exist. This is the primary
+    means of using this package.
+
+    :param layout: a Layout object which defines the fields that make up the
+        data set, along with the various rules that should be applied to
+        each one.
+    :param skip_rows: the number of rows to skip before starting evaluation.
+    :param max_errors: (optional) the maximum number of row errors to be
+        collected before halting validation of rows and raising a FileError.
+        This is used to prevent overly verbose (and mostly useless)
+        validation reports from being generated. Defaults to 100. The error
+        limit can overwritten (set to unlimited) by providing a value of -1.
+    :param dialect: (optional) Controls csv dialect parsing.
+    :param delimiter: (optional) Controls csv delimiter parsing.
+    :param quotechar: (optional) Controls csv quote character parsing.
+    """
+
+    def __init__(self, layout: Union[Layout, dict], skip_rows=0, max_errors=100, **kwargs):
+        x = {x: kwargs.pop(x, None) for x in ['dialect', 'delimiter', 'quotechar']}
+        self.csv_kwargs = {k: v for k, v in x.items() if v}
+        super().__init__(layout, skip_rows, max_errors, **kwargs)
+
+    def _rows(self, file: Path):
+        class Handler:
+            def __init__(self, file_path: Path, **kwargs):
+                self.file_path = file_path
+                self.csv_kwargs = kwargs
+
+            def __enter__(self) -> Iterable:
+                self.file_object = self.file_path.open(newline='')
+                return csv.reader(self.file_object, **self.csv_kwargs)
+
+            def __exit__(self, exc_type, exc_val, exc_tb):
+                self.file_object.close()
+
+        return Handler(file, **self.csv_kwargs)
+
+
+class ExcelFile(_BaseFile):
+    """
+    CSV File class
+
+    This class provides a way to validate the contents of a file against a
+    Layout, and report any rule violations that exist. This is the primary
+    means of using this package.
+
+    :param layout: a Layout object which defines the fields that make up the
+        data set, along with the various rules that should be applied to
+        each one.
+    :param skip_rows: the number of rows to skip before starting evaluation.
+    :param max_errors: the maximum number of row errors to be
+        collected before halting validation of rows and raising a FileError.
+        This is used to prevent overly verbose (and mostly useless)
+        validation reports from being generated. The error
+        limit can be set to unlimited by providing a value of -1.
+
+    """
+
+    def __init__(self, layout: Union[Layout, Dict], skip_rows=0, max_errors=100, **kwargs):
+        try:
+            __import__('openpyxl')
+        except ModuleNotFoundError:
+            raise ModuleNotFoundError(
+                "openpyxl not available for import. You must install this"
+                " package before you can use the ExcelFile class."
             )
-            if ce:
-                e.append(ce)
-        if e:
-            return ex.FileError(file=p.name, errors=e)
+
+        x = {x: kwargs.pop(x, None) for x in ['sheet']}
+        self.excel_kwargs = {k: v for k, v in x.items() if v}
+
+        super().__init__(layout, skip_rows, max_errors, **kwargs)
+
+    def _rows(self, file: Path, sheet=None):
+        class Handler:
+            def __init__(self, file_path, **kwargs):
+                self.file_path = file_path
+                self.excel_kwargs = kwargs
+
+            def __enter__(self) -> Iterable:
+                from openpyxl import load_workbook
+                wb = load_workbook(file)
+                sheet_name = self.excel_kwargs.get('sheet')
+                ws = wb.get_sheet_by_name(sheet_name) if sheet else wb.active
+                return ws.values
+
+            def __exit__(self, exc_type, exc_val, exc_tb):
+                pass
+
+        return Handler(file, **self.excel_kwargs)
+
+    @staticmethod
+    def _row_handler(row: list) -> List[str]:
+        return [str(x or '') for x in row]
