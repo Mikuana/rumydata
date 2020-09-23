f4e9104e2838ad52cb607115786954f517961537
Christopher Boyd <Mikuana@gmail.com>
2020-09-22 21:47:41 -0700

    Header stripper (#43)
    * checkpoint header stripper
    
    * Remove unused extra param
    
    * Corrected tests and header mode functions

--- rumydata/rules/header.py
+++ rumydata/rules/header.py
@@ -12,16 +12,17 @@ from rumydata._base import _BaseRule
 
 # this named tuple is here to allow for setting the default argument without
 # needing to import the Layout class, which results in a circular import
-_default_thing = namedtuple('DefaultDict', ['layout'])
+_default_thing = namedtuple('DefaultDict', ['layout', 'header_mode'])
 
 
 class Rule(_BaseRule):
     """ Header Rule """
 
-    _default_args = (_default_thing({}),)
+    _default_args = (_default_thing({}, 'exact'),)
 
     def __init__(self, columns):
         super().__init__()
+        self.header_mode = columns.header_mode
         self.definition = columns.layout
 
     def _prepare(self, data: List[str]) -> tuple:
@@ -32,7 +33,12 @@ class NoExtra(Rule):
     """ No extra header elements Rule """
 
     def _evaluator(self):
-        return lambda x: all([y in self.definition for y in x])
+        modes = {
+            'exact': lambda x: all([y in self.definition for y in x]),
+            'startswith': lambda x: all([any([y.startswith(z) for z in self.definition]) for y in x]),
+            'contains': lambda x: all([any([z in y for z in self.definition]) for y in x])
+        }
+        return modes[self.header_mode]
 
     def _explain(self):
         return 'Header row must not have unexpected columns'
@@ -42,7 +48,12 @@ class NoMissing(Rule):
     """ No missing header elements Rule """
 
     def _evaluator(self):
-        return lambda x: all([y in x for y in self.definition])
+        modes = {
+            'exact': lambda x: all([y in x for y in self.definition]),
+            'startswith': lambda x: all([any([z.startswith(y) for z in x]) for y in self.definition]),
+            'contains': lambda x: all([any([y in z for z in x]) for y in self.definition])
+        }
+        return modes[self.header_mode]
 
     def _explain(self) -> str:
         return 'Header row must not be missing any expected columns'
@@ -52,7 +63,20 @@ class NoDuplicate(Rule):
     """ No duplicate header elements Rule """
 
     def _evaluator(self):
-        return lambda x: len(x) == len(set(x))
+        def starts_with(x):
+            ixs = [[y.startswith(z) for z in list(self.definition.keys())].index(True) for y in x]
+            return len(ixs) == len(set(ixs))
+
+        def contains(x):
+            ixs = [[z in y for z in list(self.definition.keys())].index(True) for y in x]
+            return len(ixs) == len(set(ixs))
+
+        modes = {
+            'exact': lambda x: len(x) == len(set(x)),
+            'startswith': starts_with,
+            'contains': contains
+        }
+        return modes[self.header_mode]
 
     def _explain(self):
         return 'Header row must not contain duplicate values'
@@ -62,7 +86,20 @@ class ColumnOrder(Rule):
     """ Fixed header element order Rule """
 
     def _evaluator(self):
-        return lambda x: x == list(self.definition)
+        def starts_with(x):
+            ixs = [[y.startswith(z) for z in list(self.definition.keys())].index(True) for y in x]
+            return ixs == sorted(ixs)
+
+        def contains(x):
+            ixs = [[z in y for z in list(self.definition.keys())].index(True) for y in x]
+            return ixs == sorted(ixs)
+
+        modes = {
+            'exact': lambda x: x == list(self.definition),
+            'startswith': starts_with,
+            'contains': contains
+        }
+        return modes[self.header_mode]
 
     def _explain(self):
         return 'Header row must explicitly match order of definition'
--- rumydata/table.py
+++ rumydata/table.py
@@ -31,6 +31,10 @@ class Layout(_BaseSubject):
     :param _definition: dictionary of column names with DataType definitions
     :param skip_header: (optional) a boolean control to skip validation of
         the header in the file. Defaults to False.
+    :param header_mode: (optional) sets the mode for checking the header.
+        Defaults to 'exact', but also accepts 'startswith' and 'contains'. This
+        allows optional partial matching of header values in a layout to the
+        values obtained in tabular data.
     :param empty_row_ok: (optional) a boolean control to skip validation of
         any row that is completely empty (i.e. every field is blank).
         Defaults to False.
@@ -41,6 +45,11 @@ class Layout(_BaseSubject):
     def __init__(self, _definition: Dict[str, field.Field], **kwargs):
         self.skip_header = kwargs.pop('skip_header', False)
         self.empty_row_ok = kwargs.pop('empty_row_ok', False)
+        self.header_mode = kwargs.pop('header_mode', 'exact')
+
+        header_modes = ('exact', 'startswith', 'contains')
+        if self.header_mode not in header_modes:
+            raise ValueError(f"header_mode argument invalid. Must be one of: {header_modes}")
 
         super().__init__(**kwargs)
 
