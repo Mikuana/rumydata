cf4c706cfa91e9d922e718999615cae9e85f718c
Christopher Boyd <Mikuana@gmail.com>
2020-10-05 21:34:30 -0700

    Add GitHub link to pypi page (#44)

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
     author="Christopher Boyd",
     description="A python package for validating expectations of text data, and safely reporting exceptions",
     long_description_content_type="text/markdown",
-    url=None,
+    url="https://github.com/Mikuana/rumydata",
     long_description=Path('README.md').read_text(),
     classifiers=[
         "Programming Language :: Python :: 3",
