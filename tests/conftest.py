2a9d47ab767515537f1fc8af3f5400977f866fa8
Jacob Campbell <jacob.campbell@hca.wa.gov>
2024-05-10 11:54:28 -0700

    Conditionally ignore cleanup errors in newer versions of python tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,15 +2,20 @@ import tempfile
 from pathlib import Path
 
 import pytest
-
+import sys
 from rumydata.field import Text, Integer, Date, Choice
 
 
 @pytest.fixture()
+@pytest.mark.skipif(sys.version_info >= (3, 10))
 def tmpdir():
     with tempfile.TemporaryDirectory() as d:
         yield Path(d)
 
+@pytest.mark.skipif(sys.version_info < (3, 10))
+def tmpdir():
+    with tempfile.TemporaryDirectory(ignore_cleanup_errors=True) as d:
+        yield Path(d)
 
 @pytest.fixture()
 def basic() -> dict:
