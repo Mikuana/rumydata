47e98e40964cb46da3d78e2bd8909879d18f6e07
Jacob Campbell <jacob.campbell@hca.wa.gov>
2024-05-10 12:02:08 -0700

    Revert "Revert "Ignore cleanup errors for temp directories""
    This reverts commit 134997d2d4aa031792c6ad2a132b475e10db6377.

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ from rumydata.field import Text, Integer, Date, Choice
 
 @pytest.fixture()
 def tmpdir():
-    with tempfile.TemporaryDirectory() as d:
+    with tempfile.TemporaryDirectory(ignore_cleanup_errors=True) as d:
         yield Path(d)
 
 
