29361b4cab9714926129c28d089d50c024a9e184
Jacob Campbell <jacob.campbell@hca.wa.gov>
2024-05-10 12:00:35 -0700

    Mark extra function as a fixture....

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,6 +12,8 @@ def tmpdir():
     with tempfile.TemporaryDirectory() as d:
         yield Path(d)
 
+
+@pytest.fixture()
 @pytest.mark.skipif(sys.version_info < (3, 10))
 def tmpdir():
     with tempfile.TemporaryDirectory(ignore_cleanup_errors=True) as d:
